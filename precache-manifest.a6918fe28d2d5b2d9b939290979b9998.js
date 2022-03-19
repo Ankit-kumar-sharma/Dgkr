b927c924176e824d920fb90e68b7963cf099c3eb
Ankit-kumar-sharma <ankitkumarsharmacse@gmail.com>
2022-03-19 13:51:24 +0530

    Updates

---
+++ precache-manifest.a6918fe28d2d5b2d9b939290979b9998.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e59cb97440352a5b9a632145f77587f",
+    "url": "/Dgkr/index.html"
+  },
+  {
+    "revision": "7e61b746e9d3e8d78027",
+    "url": "/Dgkr/static/css/main.047585df.chunk.css"
+  },
+  {
+    "revision": "f5b2992ec32e20c0d074",
+    "url": "/Dgkr/static/js/2.f03ee4f7.chunk.js"
+  },
+  {
+    "revision": "7e61b746e9d3e8d78027",
+    "url": "/Dgkr/static/js/main.3f7123bf.chunk.js"
+  },
+  {
+    "revision": "53a34378ac2312659c2d",
+    "url": "/Dgkr/static/js/runtime-main.c4b8321b.js"
+  },
+  {
+    "revision": "2490d7073008225813244032b41f61fc",
+    "url": "/Dgkr/static/media/banner.2490d707.jpg"
+  },
+  {
+    "revision": "66fbda9447aa2b7c4e3adc89cf3eb7e0",
+    "url": "/Dgkr/static/media/logo.66fbda94.png"
+  }
+]);
